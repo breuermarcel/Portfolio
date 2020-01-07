0ad7af60b881ab59c4526007a48d5ad79eeacf11
Marcel Breuer <breuer.marcel@outloook.com>
2020-01-07 09:41:23 +0100

    Initial commit

---
+++ dist/js/navbar.js
@@ -0,0 +1,37 @@
+(function(jQuery) {
+    "use strict";
+    jQuery('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = jQuery(this.hash);
+            target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                jQuery('html, body').animate({
+                    scrollTop: (target.offset().top - 70)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+    jQuery('.js-scroll-trigger').click(function() {
+        jQuery('.navbar-collapse').collapse('hide');
+    });
+    jQuery('body').scrollspy({
+        target: '#mainNav',
+        offset: 100
+    });
+    var navbarCollapse = function() {
+        if (jQuery(window).width() > 475) {
+            if (jQuery("#mainNav").offset().top > 100) {
+                jQuery("#mainNav").addClass("navbar-shrink");
+                jQuery("#navbar-logo").attr("src", "./dist/img/Black_Logo.svg");
+            } else {
+                jQuery("#mainNav").removeClass("navbar-shrink");
+                jQuery("#navbar-logo").attr("src", "./dist/img/White_Logo.svg");
+            }
+        } else {
+            jQuery("#navbar-logo").attr("src", "./dist/img/Black_Logo.svg");
+        }
+    };
+    navbarCollapse();
+    jQuery(window).scroll(navbarCollapse);
+})(jQuery);
